c338285ef5ba255ed960370e4d8be9279a06319a
rajesh-echologyx <rajesh@echologyx.com>
2024-12-09 17:00:50 +0600

    updated changes for deoplyment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv';
 
-// https://vite.dev/config/
+dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
+
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 3000,
+  },
+});
